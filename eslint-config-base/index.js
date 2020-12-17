58dc1f8844ed9e44c91e4eb500c2b9970750cb0f
royyan-kargo <royyan@kargo.tech>
2020-12-17 20:30:31 +0700

    Fix more weird character

--- eslint-config-base/index.js
+++ eslint-config-base/index.js
@@ -13,9 +13,9 @@ module.exports = {
         'spec/**', // mocha, rspec-like pattern
         '**/__tests__/**', // jest pattern
         '**/__mocks__/**', // jest pattern
-        'test.{​​​​​​​js,jsx}​​​​​​​', // repos with a single test file
-        'test-*.{​​​​​​​js,jsx}​​​​​​​', // repos with multiple top-level test files
-        '**/*{​​​​​​​.,_}​​​​​​​{​​​​​​​test,spec}​​​​​​​.{​​​​​​​js,jsx}​​​​​​​', // tests where the extension or filename suffix denotes that it is a test
+        'test.{js,jsx}', // repos with a single test file
+        'test-*.{js,jsx}', // repos with multiple top-level test files
+        '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
         '**/jest.config.js', // jest config
         '**/jest.setup.js', // jest setup
         '**/vue.config.js', // vue-cli config
@@ -25,12 +25,12 @@ module.exports = {
         '**/rollup.config.*.js', // rollup config
         '**/gulpfile.js', // gulp config
         '**/gulpfile.*.js', // gulp config
-        '**/Gruntfile{​​​​​​​,.js}​​​​​​​', // grunt config
+        '**/Gruntfile{,.js}', // grunt config
         '**/protractor.conf.js', // protractor config
         '**/protractor.conf.*.js', // protractor config
         '**/karma.conf.js', // karma config
         '**/.eslintrc.js', // eslint config
-        '**/*.stories.{​​​​​​​js,jsx}​​​​​​​', // storybook files
+        '**/*.stories.{js,jsx}', // storybook files
       ],
       }
     ],
